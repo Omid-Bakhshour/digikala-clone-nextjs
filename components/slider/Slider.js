4e837cc43c84d3870b4a14462009539767a93591
Omid-Bakhshour <fishvardroid@gmail.com>
2022-01-22 20:23:05 +0400

    complete slider

--- components/slider/Slider.js
+++ components/slider/Slider.js
@@ -2,65 +2,32 @@
 import ArrowLeft from "@heroicons/react/outline/ChevronLeftIcon"
 import SliderItem from "./SliderItem";
 import ArrowRight from "@heroicons/react/outline/ChevronRightIcon"
-
 import { useState, useRef, useEffect } from "react"
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { EffectFade } from 'swiper';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+
+
 
 function Slider({ bgColor, sliderImg }) {
-
-
-    // count scroll next
-    const [countNavigation, setCountNavigation] = useState(0);
-    const [translateX, setTranslateX] = useState(0);
-
-    // allow number to scroll next
-    const [countSlider, setCountSlider] = useState(0);
-    const ElementRef = useRef(null)
-
-
-    const styles = {
-        transform: `translate3d(${translateX}px, 0,0)`
-    };
-
-    useEffect(() => {
-        const Elementcount = ElementRef.current.childNodes.length;
-        setCountSlider((Elementcount / 4) - 1);
-
-    }, [])
-
-
-    useEffect(() => {
-        const setNavigation = () => {
-
-            var sliderIndex = countNavigation;
-
-            if (sliderIndex > countSlider) {
-                sliderIndex = 0
-                setCountNavigation(0)
-            }
-
-            if (sliderIndex < 0) {
-                sliderIndex = countSlider
-                setCountNavigation(countSlider)
-
-            }
-
-
-            setTranslateX(countNavigation * 4 * 267)
-        }
-        setNavigation()
-    }, [countNavigation])
 
 
     return (
         <div style={{ backgroundColor: bgColor }} className="my-4 max-w-[1366px] 2xl:mx-4 " >
 
-            <section className="py-9 px-4 flex flex-row-reverse items-center justify-center   " >
+            <section className="py-9 px-4 flex md:flex-row-reverse items-center justify-center flex-col gap-y-3  " >
 
-                <a className=" flex-col my-0 mx-11 w-[170px] cursor-pointer group hidden sm:flex"  >
+                <a className=" flex-col my-0 mx-11 w-[170px] cursor-pointer group "  >
                     <img src={sliderImg} className="w-full h-[calc(100%-41px)]" />
 
-                    <div className="flex flex-row items-center justify-center gap-x-2 text-white py-2 px-4
-                     rounded-md border border-white " >
+                    <div className=" flex-row items-center justify-center gap-x-2 text-white py-2 px-4
+                     rounded-md border border-white hidden sm:flex " >
 
                         <ArrowLeft className="w-4 h-4 group-hover:animate-bounce" />
 
@@ -70,50 +37,72 @@ function Slider({ bgColor, sliderImg }) {
 
                 {/* slider */}
 
-                <div className="flex flex-1 w-[calc(267px-8px)] max-w-[calc(267px-8px)] md:w-[calc(534px-8px)] md:max-w-[calc(534px-8px)] lg:w-[calc(801px-8px)] lg:max-w-[calc(801px-8px)]
-                xl:w-[calc(100%)] rounded-lg xl:max-w-[calc(100%)]   overflow-hidden   ">
+
+
+                <div className="flex flex-1 w-full lg  overflow-hidden   ">
 
                     <div className=" w-full   transform  relative  "  >
 
-                        <div style={styles} className=" w-full slider-type1 transition-all duration-1000 ease-in-out 
-                     flex-row-reverse flex  justify-start gap-x-2   " ref={ElementRef} >
 
-                            <SliderItem title="اسپیکر بلوتوثی قابل حمل کینگ استار مدل KBS 250" img="/images/slider21.jpg" price="719000" discount="32" time="2022-01-22 00:00:00" remained="71" />
-                            <SliderItem title="اسپیکر بلوتوثی قابل حمل کینگ استار مدل KBS 250" img="/images/slider22.jpg" price="719000" discount="32" time="2022-01-22 00:00:00" remained="71" />
-                            <SliderItem title="اسپیکر بلوتوثی قابل حمل کینگ استار مدل KBS 250" img="/images/slider23.jpg" price="719000" discount="32" time="2022-01-22 00:00:00" remained="71" />
-                            <SliderItem title="اسپیکر بلوتوثی قابل حمل کینگ استار مدل KBS 250" img="/images/slider24.jpg" price="719000" discount="32" time="2022-01-22 00:00:00" remained="71" />
-                            <SliderItem title="اسپیکر بلوتوثی قابل حمل کینگ استار مدل KBS 250" img="/images/slider24.jpg" price="719000" discount="32" time="2022-01-22 00:00:00" remained="71" />
-                            <SliderItem title="اسپیکر بلوتوثی قابل حمل کینگ استار مدل KBS 250" img="/images/slider23.jpg" price="719000" discount="32" time="2022-01-22 00:00:00" remained="71" />
-                            <SliderItem title="اسپیکر بلوتوثی قابل حمل کینگ استار مدل KBS 250" img="/images/slider22.jpg" price="719000" discount="32" time="2022-01-22 00:00:00" remained="71" />
-                            <SliderItem title="اسپیکر بلوتوثی قابل حمل کینگ استار مدل KBS 250" img="/images/slider21.jpg" price="719000" discount="32" time="2022-01-22 00:00:00" remained="71" />
-                            <SliderItem title="اسپیکر بلوتوثی قابل حمل کینگ استار مدل KBS 250" img="/images/slider21.jpg" price="719000" discount="32" time="2022-01-22 00:00:00" remained="71" />
+                        <Swiper dir="rtl"
+                            modules={[Navigation, Pagination, Scrollbar, A11y, EffectFade]}
+                            spaceBetween={8}
+                            navigation
+                            breakpoints={{
+                                // when window width is >= 640px
+                                1200: {
+
+                                    slidesPerView: 4,
+                                },
+                                // when window width is >= 768px
+                                1000: {
+                                    slidesPerView: 3,
+                                },
+
+                                768: {
+                                    slidesPerView: 2,
+                                },
+
+                                400: {
+                                    slidesPerView: 1,
+                                },
+                            }}>
+                            <SwiperSlide>
+                                <SliderItem title="اسپیکر بلوتوثی قابل حمل کینگ استار مدل KBS 250" img="/images/slider21.jpg" price="719000" discount="32" time="2022-01-22 00:00:00" remained="71" />
+
+                            </SwiperSlide>
+                            <SwiperSlide>
+                                <SliderItem title="اسپیکر بلوتوثی قابل حمل کینگ استار مدل KBS 250" img="/images/slider22.jpg" price="719000" discount="32" time="2022-01-22 00:00:00" remained="71" />
+
+                            </SwiperSlide>
+                            <SwiperSlide>
+                                <SliderItem title="اسپیکر بلوتوثی قابل حمل کینگ استار مدل KBS 250" img="/images/slider23.jpg" price="719000" discount="32" time="2022-01-22 00:00:00" remained="71" />
+
+                            </SwiperSlide>
+                            <SwiperSlide>
+                                <SliderItem title="اسپیکر بلوتوثی قابل حمل کینگ استار مدل KBS 250" img="/images/slider24.jpg" price="719000" discount="32" time="2022-01-22 00:00:00" remained="71" />
+
+                            </SwiperSlide>
 
 
-                            <SliderItem title="اسپیکر بلوتوثی قابل حمل کینگ استار مدل KBS 250" img="/images/slider21.jpg" price="719000" discount="32" time="2022-01-22 00:00:00" remained="71" />
-                            <SliderItem title="اسپیکر بلوتوثی قابل حمل کینگ استار مدل KBS 250" img="/images/slider21.jpg" price="719000" discount="32" time="2022-01-22 00:00:00" remained="71" />
-                            <SliderItem title="اسپیکر بلوتوثی قابل حمل کینگ استار مدل KBS 250" img="/images/slider21.jpg" price="719000" discount="32" time="2022-01-22 00:00:00" remained="71" />
+                            <SwiperSlide>
+                                <SliderItem title="اسپیکر بلوتوثی قابل حمل کینگ استار مدل KBS 250" img="/images/slider24.jpg" price="719000" discount="32" time="2022-01-22 00:00:00" remained="71" />
+
+                            </SwiperSlide>
+
+                            <SwiperSlide>
+                                <SliderItem title="اسپیکر بلوتوثی قابل حمل کینگ استار مدل KBS 250" img="/images/slider23.jpg" price="719000" discount="32" time="2022-01-22 00:00:00" remained="71" />
+
+                            </SwiperSlide>
 
 
 
 
-
-                        </div>
-
-                        <ArrowLeft className="next1  " onClick={() => setCountNavigation(countNavigation + 1)} />
-                        <ArrowRight className="prev1 " onClick={() => setCountNavigation(countNavigation - 1)} />
+                        </Swiper>
 
 
                     </div>
                 </div>
-
-
-
-
-
-
-
-
-
 
             </section>
 
--- components/slider/SliderItem.js
+++ components/slider/SliderItem.js
@@ -1,12 +1,50 @@
 
+import StarIcon from "@heroicons/react/outline/SparklesIcon"
+import ClockIcon from "@heroicons/react/outline/ClockIcon"
 
 function SliderItem({ title, img, price, discount, time, remained }) {
     return (
-        <div className="relative w-[259px] min-w-[259px] h-full flex bg-white rounded-lg  z-[1] " >
+        <div className="relative w-full  h-full flex bg-white rounded-lg  z-[1] " >
             <a className=" min-h-[444px]  flex flex-col items-center pt-11 px-4 pb-4 w-full " >
                 <div className=" pt-2 w-[180px] h-[180px] flex items-center " >
                     <img src={img} />
                 </div>
+                <div className="text-right mt-[14px] h-11 min-h[50px] w-full text-[#535353] text-xs max-w-[90%] overflow-hidden">{title}</div>
+
+                <div className="flex flex-row w-full text-right items-center mt-[10px]">
+                    <StarIcon className="w-[18px] h-[18px] text-[#a63489] " />
+                    <span className="text-[#62666d] text-xs mr-2" >۱,۰۰۰ تومان هدیه نقدی</span>
+                </div>
+
+
+                <div className="flex flex-col h-11 mt-[14px] mb-[10px]  w-full gap-y-1" >
+
+                    {/* top */}
+
+                    <div className="flex flex-row gap-x-2 items-center justify-end">
+                        <span className="text-[#acacac] line-through text-base font-normal ">{price}</span>
+
+                        <span className="bg-[#fb3449] font-bold text-white py-1 px-[10px] text-base min-w-[43px] h-6 text-center flex items-center
+                        rounded-full" >۲۴٪</span>
+                    </div>
+
+                    {/* bottom */}
+
+                    <div className="w-full flex flex-row gap-x-1 justify-end text-[#000] text-lg font-bold items-center" >
+                        {price}
+                        <span className="text-[#000] text-xs font-normal" >تومان</span>
+
+                    </div>
+
+
+
+                </div>
+
+                <div className="w-full mt-5 rounded-full h-1 bg-[#e2e2e2] flex justify-end overflow-hidden" >
+                    <div className="bg-[linear-gradient(90deg,#ff5b79,#fb3449)] w-[92%] h-full block " />
+                </div>
+
+
 
             </a>
 
